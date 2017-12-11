6b01331fa715f636c45613cceafc51db90457046
Glitch (childlike-carol) <none>
2017-12-11 04:18:28 +0000

    💠🎸 Checkpoint sketch.js:537253

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var block = 32;
 var ground;
+var spr;
 var squareGroundImg;
 var level = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
              0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -23,26 +24,47 @@ function preload() {
 }
 
 function setup() {
+  noSmooth();
   var cnv = createCanvas(512,480);
   cnv.parent('sketch-holder');
   
   ground = new Group();
-  
   for (var i =0; i<15; i++){
     for (var j =0; j<16; j++){
       if (level[i*16+j]){
-        var temp  = createSprite(j*block, i*block, block, block);
+        var temp  = createSprite(j*block +block/2, i*block +block/2, block, block);
+        squareGroundImg.resize(block, 0);
         temp.addImage(squareGroundImg);
         ground.add(temp);
-      }
-        
+      }   
     }
   }
   
-  //createSprite(width/2, height/2, 50, 50);
+  spr = createSprite(width/2, height/2, block, block);
+  spr.shapeColor = color(255);
+  spr.velocity.y = 0.5;
 }
 
 function draw() {
   background(0,0,0);  
   drawSprites();
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    spr.setSpeed(1.5, 0);
+  }
+  else if (keyCode == DOWN_ARROW) {
+    spr.setSpeed(1.5, 90);
+  }
+  else if (keyCode == LEFT_ARROW) {
+    spr.setSpeed(1.5, 180);
+  }
+  else if (keyCode == UP_ARROW) {
+    spr.setSpeed(1.5, 270);
+  }
+  else if (key == ' ') {
+    spr.setSpeed(0, 0);
+  }
+  return false;
 }
