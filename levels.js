9a67aa8ecf3a60f19e5794245e2e19d7e5610231
Glitch (childlike-carol) <none>
2017-12-15 02:21:07 +0000

    🏰📼 Checkpoint sketch.js:537253

--- levels.js
+++ levels.js
@@ -1,3 +1,29 @@
+function loadLevel(name,x,y){
+  //platforms
+  ground.clear();
+  var temp = levels[name].ground;
+  for (var i=0; i<temp.length;i+=4){
+    ground.add( createPlatform(temp[i],temp[i+1],temp[i+2],temp[i+3], squareGroundImg) );
+  }
+
+  //ladders
+  ladders.clear();
+  var temp = levels[name].ladders;
+  for (var i=0; i<temp.length;i+=3){
+    ladders.add( createLadder(temp[i].x,temp[i+1].y,temp[i+2].h, ladderImg) );
+  }
+  //doors
+  doors.clear();
+  var temp = levels[name].doors;
+  for (var i=0; i<temp.length;i+=5){
+    doors.add( createDoor(temp[i],temp[i+1],temp[i+2],temp[i+3],temp[i+4]) );
+  }
+  
+  //hero
+  spr.position.x = (x+0.5)*block;
+  spr.position.y = (y-0.5)*block;
+}
+
 var levels = {};
 
 levels['1.0'] ={
--- sketch.js
+++ sketch.js
@@ -235,29 +235,3 @@ function loadHero() {
   spr.height = block*2;
   hero.add(spr);
 }
-
-function loadLevel(name,x,y){
-  //platforms
-  ground.clear();
-  var temp = levels[name].ground;
-  for (var i=0; i<temp.length;i+=4){
-    ground.add( createPlatform(temp[i],temp[i+1],temp[i+2],temp[i+3], squareGroundImg) );
-  }
-
-  //ladders
-  ladders.clear();
-  var temp = levels[name].ladders;
-  for (var i=0; i<temp.length;i+=3){
-    ladders.add( createLadder(temp[i].x,temp[i+1].y,temp[i+2].h, ladderImg) );
-  }
-  //doors
-  doors.clear();
-  var temp = levels[name].doors;
-  for (var i=0; i<temp.length;i+=5){
-    doors.add( createDoor(temp[i],temp[i+1],temp[i+2],temp[i+3],temp[i+4]) );
-  }
-  
-  //hero
-  spr.position.x = (x+0.5)*block;
-  spr.position.y = (y-0.5)*block;
-}
